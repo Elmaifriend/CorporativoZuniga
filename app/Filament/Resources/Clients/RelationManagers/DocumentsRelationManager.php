8ee2d278395b468502d24d46c2d55f70db875f90
Elmaifriend <fgp332@gmail.com>
2025-12-15 10:37:59 -0800

    Added more options to FileName filed in ClientDocuemntsRelationManager

--- app/Filament/Resources/Clients/RelationManagers/DocumentsRelationManager.php
+++ app/Filament/Resources/Clients/RelationManagers/DocumentsRelationManager.php
@@ -17,6 +17,7 @@ use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Schemas\Schema;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
+use Filament\Forms\Components\Select;
 
 class DocumentsRelationManager extends RelationManager
 {
@@ -29,11 +30,58 @@ class DocumentsRelationManager extends RelationManager
         return $schema
             ->columns(2) // 2 columnas para mejor diseño
             ->components([
-                TextInput::make('document_name')
+                Select::make('document_name')
                     ->label('Tipo de documento')
                     ->columnSpanFull()
                     ->required()
-                    ->placeholder('Ej. Acta Constitutiva'),
+                    ->placeholder('Selecciona un tipo de documento')
+                    ->options([
+                        // Documentos personales
+                        'acta_nacimiento' => 'Acta de nacimiento',
+                        'pasaporte' => 'Pasaporte',
+                        'curp' => 'CURP',
+                        'identificacion_oficial' => 'Identificación oficial',
+                        'comprobante_domicilio' => 'Comprobante de domicilio',
+                        'acta_matrimonio' => 'Acta de matrimonio',
+                        'acta_divorcio' => 'Acta de divorcio',
+                        'acta_defuncion' => 'Acta de defunción',
+
+                        // Migración y USCIS / INM
+                        'formato_i130' => 'Petición familiar (I-130)',
+                        'formato_i485' => 'Ajuste de estatus (I-485)',
+                        'formato_i601' => 'Perdón migratorio (I-601)',
+                        'formato_i601a' => 'Perdón provisional (I-601A)',
+                        'formato_i765' => 'Permiso de trabajo (I-765)',
+                        'formato_i131' => 'Permiso de viaje (I-131)',
+                        'formato_i864' => 'Declaración de patrocinio (I-864)',
+                        'formato_i589' => 'Solicitud de asilo (I-589)',
+                        'formato_i90' => 'Renovación de residencia (I-90)',
+                        'formato_n400' => 'Naturalización (N-400)',
+                        'formato_n600' => 'Certificado de ciudadanía (N-600)',
+
+                        // Visas
+                        'visa_turista' => 'Visa de turista',
+                        'visa_estudiante' => 'Visa de estudiante',
+                        'visa_trabajo' => 'Visa de trabajo',
+                        'visa_prometido' => 'Visa de prometido/a (K-1)',
+                        'visa_inversionista' => 'Visa de inversionista',
+                        'visa_humanitaria' => 'Visa humanitaria',
+
+                        // Soporte legal
+                        'carta_explicativa' => 'Carta explicativa',
+                        'carta_empleador' => 'Carta del empleador',
+                        'antecedentes_penales' => 'Carta de antecedentes penales',
+                        'pruebas_relacion' => 'Pruebas de relación familiar',
+                        'pruebas_residencia' => 'Pruebas de residencia',
+                        'traduccion_certificada' => 'Traducción certificada',
+                        'poder_notarial' => 'Poder notarial',
+                        'resolucion_migratoria' => 'Resolución migratoria',
+                        'notificacion_uscis' => 'Notificación de USCIS',
+
+                        // Última opción
+                        'otro' => 'Otro',
+                    ])
+                    ->searchable(),
 
                 FileUpload::make('document_path')
                     ->label('Archivo PDF')
